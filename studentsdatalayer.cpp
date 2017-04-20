ccb5b2a9927c9c8637eac4d34cf68c1f705ba3d8
Dudarev Ilia <dudareviv@gmail.com>
2017-04-20 17:18:11 +0400

    Изменил тип работы с файлом на бинарный.

--- studentsdatalayer.cpp
+++ studentsdatalayer.cpp
@@ -21,19 +21,17 @@ int StudentsDataLayer::load(){
 
     QLinkedList<Student> loaded_data;
 
-    QTextStream in(&file);
-    //QDataStream in(&file);
-    //in.setVersion(QDataStream::Qt_5_0);
+    QDataStream in(&file);
+    in.setVersion(QDataStream::Qt_5_0);
 
     while(!in.atEnd()){
-        QString line = in.readLine();
-        QStringList values = line.split("\t");
+        Student *student = new Student();
 
-        if(values.count() != Student::headers().count()){
-            return LOAD_ERROR_FORMAT;
+        QLinkedList<QString*> values = student->values();
+        for(QLinkedList<QString*>::iterator value = values.begin(); value != values.end(); value++){
+            in >> **value;
         }
 
-        Student *student = new Student(values[0], values[1], values[2], values[3]);
         loaded_data.append(*student);
     }
 
@@ -53,22 +51,15 @@ int StudentsDataLayer::save(){
         return SAVE_ERROR_OPEN;
     }
 
-    QTextStream out(&file);
-    //QDataStream out(&file);
-    //out.setVersion(QDataStream::Qt_5_0);
+    QDataStream out(&file);
+    out.setVersion(QDataStream::Qt_5_0);
 
     for(QLinkedList<Student>::iterator student = data.begin(); student != data.end(); student++){
         QLinkedList<QString*> values = student->values();
 
         for(QLinkedList<QString*>::iterator value = values.begin(); value != values.end(); value++){
             out << **value;
-
-            if(value != values.end() - 1)
-                out << "\t";
         }
-
-        if(student != data.end() - 1)
-            out << "\n";
     }
 
     file.flush();
